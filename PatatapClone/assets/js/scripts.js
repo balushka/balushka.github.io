8a1d7d2717a2cd5666a25a719b61afde2493e5cf
balushka <64082351+balushka@users.noreply.github.com>
2020-06-04 14:56:25 +0200

    patatap refactored

--- PatatapClone/assets/js/scripts.js
+++ PatatapClone/assets/js/scripts.js
@@ -3,13 +3,142 @@ paper.install(window);
 
 window.onload = function () {
     paper.setup('myCanvas');
-    // view.draw();
     var tool = new Tool();
-    var goodKeys = [65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90, 97, 98, 99, 100, 101, 102, 103, 104, 105, 106, 107, 108, 109, 110, 111, 112, 113, 114, 115, 116, 117, 118, 119, 120, 121, 122];
-    // var goodLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "w", "x", "y", "z"]
-    var pattern = new RegExp( "[a-zA-Z]{1}");
     var randomInInterval = function (min, max) {
         return Math.floor(Math.random() * (max - min + 1) + min);
+    }
+
+    var keyData = {
+        a: {
+            sound: new Howl({
+                src: ['sounds/bubbles.mp3']
+            })
+        },
+        b: {
+            sound: new Howl({
+                src: ['sounds/clay.mp3']
+            })
+        },
+        c: {
+            sound: new Howl({
+                src: ['sounds/confetti.mp3']
+            })
+        },
+        d: {
+            sound: new Howl({
+                src: ['sounds/corona.mp3']
+            })
+        },
+        e: {
+            sound: new Howl({
+                src: ['sounds/dotted-spiral.mp3']
+            })
+        },
+        f: {
+            sound: new Howl({
+                src: ['sounds/flash-1.mp3']
+            })
+        },
+        g: {
+            sound: new Howl({
+                src: ['sounds/flash-2.mp3']
+            })
+        },
+        h: {
+            sound: new Howl({
+                src: ['sounds/flash-3.mp3']
+            })
+        },
+        i: {
+            sound: new Howl({
+                src: ['sounds/glimmer.mp3']
+            })
+        },
+        j: {
+            sound: new Howl({
+                src: ['sounds/moon.mp3']
+            })
+        },
+        k: {
+            sound: new Howl({
+                src: ['sounds/pinwheel.mp3']
+            })
+        },
+        l: {
+            sound: new Howl({
+                src: ['sounds/piston-1.mp3']
+            })
+        },
+        m: {
+            sound: new Howl({
+                src: ['sounds/piston-2.mp3']
+            })
+        },
+        n: {
+            sound: new Howl({
+                src: ['sounds/piston-3.mp3']
+            })
+        },
+        o: {
+            sound: new Howl({
+                src: ['sounds/prism-1.mp3']
+            })
+        },
+        p: {
+            sound: new Howl({
+                src: ['sounds/prism-2.mp3']
+            })
+        },
+        q: {
+            sound: new Howl({
+                src: ['sounds/splits.mp3']
+            })
+        },
+        r: {
+            sound: new Howl({
+                src: ['sounds/squiggle.mp3']
+            })
+        },
+        s: {
+            sound: new Howl({
+                src: ['sounds/strike.mp3']
+            })
+        },
+        t: {
+            sound: new Howl({
+                src: ['sounds/suspension.mp3']
+            })
+        },
+        u: {
+            sound: new Howl({
+                src: ['sounds/timer.mp3']
+            })
+        },
+        v: {
+            sound: new Howl({
+                src: ['sounds/ufo.mp3']
+            })
+        },
+        w: {
+            sound: new Howl({
+                src: ['sounds/veil.mp3']
+            })
+        },
+        x: {
+            sound: new Howl({
+                src: ['sounds/prism-3.mp3']
+            })
+        },
+        y: {
+            sound: new Howl({
+                src: ['sounds/wipe.mp3']
+            })
+        },
+        z: {
+            sound: new Howl({
+                src: ['sounds/zig-zag.mp3']
+            })
+        }
     }
 
     function createRndPolygon() {
@@ -37,300 +166,26 @@ window.onload = function () {
         return "rgb(" + redBg + ", " + greenBg + ", " + blueBg + ")";
     }
 
-    $(document).on("click", function() {
-        $('#dummy').focus();
-      });
-      $('#dummy').focus();
-
-    $("body").on("keypress", function (event) {
-        console.log(event.which);
-        //Create random body background color:
-        if (goodKeys.includes(event.which)) {
-            $(this).css("background", createRndBgColor());
-            $("#hint").fadeOut(2000, function () { this.remove() });
-
-            // Create random sided, sized and colored polygons on keydown
-            tool.onKeyDown = function (event) {
-                // if (goodLetters.includes(event.key) && event.key.length == 1) {
-                if (pattern.test(event.key) && event.key.length == 1) {
-                    console.log(event.key.length);
-                    var polygon = createRndPolygon();
-                    polygon.onFrame = function (e) {
-                        // On each frame, decrease the size to 90%:
-                        polygon.scale(0.9);
-                    }
+    tool.onKeyDown = function (event) {
+        if (keyData[event.key]) {
+            var polygon = createRndPolygon();
+            polygon.onFrame = function (e) {
+                // On each frame, decrease the size to 95%:
+                polygon.scale(0.95);
+                polygon.fillColor.hue += 2;
+                // if polygon is samell we don't need it anymore
+                if (polygon.area < 1) {
+                    polygon.remove();
                 }
             }
-
-
-            switch (event.which) {
-                
-                case 65:
-                    var sound = new Howl({
-                        src: ['sounds/bubbles.mp3']
-                    });
-                    break;
-                case 66:
-                    var sound = new Howl({
-                        src: ['sounds/clay.mp3']
-                    });
-                    break;
-                case 67:
-                    var sound = new Howl({
-                        src: ['sounds/confetti.mp3']
-                    });
-                    break;
-                case 68:
-                    var sound = new Howl({
-                        src: ['sounds/corona.mp3']
-                    });
-                    break;
-                case 69:
-                    var sound = new Howl({
-                        src: ['sounds/dotted-spiral.mp3']
-                    });
-                    break;
-                case 70:
-                    var sound = new Howl({
-                        src: ['sounds/flash-1.mp3']
-                    });
-                    break;
-                case 71:
-                    var sound = new Howl({
-                        src: ['sounds/flash-2.mp3']
-                    });
-                    break;
-                case 72:
-                    var sound = new Howl({
-                        src: ['sounds/flash-3.mp3']
-                    });
-                    break;
-                case 73:
-                    var sound = new Howl({
-                        src: ['sounds/glimmer.mp3']
-                    });
-                    break;
-                case 74:
-                    var sound = new Howl({
-                        src: ['sounds/moon.mp3']
-                    });
-                    break;
-                case 75:
-                    var sound = new Howl({
-                        src: ['sounds/pinwheel.mp3']
-                    });
-                    break;
-                case 76:
-                    var sound = new Howl({
-                        src: ['sounds/piston-1.mp3']
-                    });
-                    break;
-                case 77:
-                    var sound = new Howl({
-                        src: ['sounds/piston-2.mp3']
-                    });
-                    break;
-                case 78:
-                    var sound = new Howl({
-                        src: ['sounds/piston-3.mp3']
-                    });
-                    break;
-                case 79:
-                    var sound = new Howl({
-                        src: ['sounds/prism-1.mp3']
-                    });
-                    break;
-                case 80:
-                    var sound = new Howl({
-                        src: ['sounds/prism-2.mp3']
-                    });
-                    break;
-                case 81:
-                    var sound = new Howl({
-                        src: ['sounds/prism-3.mp3']
-                    });
-                    break;
-                case 82:
-                    var sound = new Howl({
-                        src: ['sounds/splits.mp3']
-                    });
-                    break;
-                case 83:
-                    var sound = new Howl({
-                        src: ['sounds/squiggle.mp3']
-                    });
-                    break;
-                case 84:
-                    var sound = new Howl({
-                        src: ['sounds/strike.mp3']
-                    });
-                    break;
-                case 85:
-                    var sound = new Howl({
-                        src: ['sounds/suspension.mp3']
-                    });
-                    break;
-                case 86:
-                    var sound = new Howl({
-                        src: ['sounds/timer.mp3']
-                    });
-                    break;
-                case 87:
-                    var sound = new Howl({
-                        src: ['sounds/ufo.mp3']
-                    });
-                    break;
-                case 88:
-                    var sound = new Howl({
-                        src: ['sounds/veil.mp3']
-                    });
-                    break;
-                case 89:
-                    var sound = new Howl({
-                        src: ['sounds/wipe.mp3']
-                    });
-                    break;
-                case 90:
-                    var sound = new Howl({
-                        src: ['sounds/zig-zag.mp3']
-                    });
-                    break;
-                case 97:
-                    var sound = new Howl({
-                        src: ['sounds/bubbles.mp3']
-                    });
-                    break;
-                case 98:
-                    var sound = new Howl({
-                        src: ['sounds/clay.mp3']
-                    });
-                    break;
-                case 99:
-                    var sound = new Howl({
-                        src: ['sounds/confetti.mp3']
-                    });
-                    break;
-                case 100:
-                    var sound = new Howl({
-                        src: ['sounds/corona.mp3']
-                    });
-                    break;
-                case 101:
-                    var sound = new Howl({
-                        src: ['sounds/dotted-spiral.mp3']
-                    });
-                    break;
-                case 102:
-                    var sound = new Howl({
-                        src: ['sounds/flash-1.mp3']
-                    });
-                    break;
-                case 103:
-                    var sound = new Howl({
-                        src: ['sounds/flash-2.mp3']
-                    });
-                    break;
-                case 104:
-                    var sound = new Howl({
-                        src: ['sounds/flash-3.mp3']
-                    });
-                    break;
-                case 105:
-                    var sound = new Howl({
-                        src: ['sounds/glimmer.mp3']
-                    });
-                    break;
-                case 106:
-                    var sound = new Howl({
-                        src: ['sounds/moon.mp3']
-                    });
-                    break;
-                case 107:
-                    var sound = new Howl({
-                        src: ['sounds/pinwheel.mp3']
-                    });
-                    break;
-                case 108:
-                    var sound = new Howl({
-                        src: ['sounds/piston-1.mp3']
-                    });
-                    break;
-                case 109:
-                    var sound = new Howl({
-                        src: ['sounds/piston-2.mp3']
-                    });
-                    break;
-                case 110:
-                    var sound = new Howl({
-                        src: ['sounds/piston-3.mp3']
-                    });
-                    break;
-                case 111:
-                    var sound = new Howl({
-                        src: ['sounds/prism-1.mp3']
-                    });
-                    break;
-                case 112:
-                    var sound = new Howl({
-                        src: ['sounds/prism-2.mp3']
-                    });
-                    break;
-                case 113:
-                    var sound = new Howl({
-                        src: ['sounds/prism-3.mp3']
-                    });
-                    break;
-                case 114:
-                    var sound = new Howl({
-                        src: ['sounds/splits.mp3']
-                    });
-                    break;
-                case 115:
-                    var sound = new Howl({
-                        src: ['sounds/squiggle.mp3']
-                    });
-                    break;
-                case 116:
-                    var sound = new Howl({
-                        src: ['sounds/strike.mp3']
-                    });
-                    break;
-                case 117:
-                    var sound = new Howl({
-                        src: ['sounds/suspension.mp3']
-                    });
-                    break;
-                case 118:
-                    var sound = new Howl({
-                        src: ['sounds/timer.mp3']
-                    });
-                    break;
-                case 119:
-                    var sound = new Howl({
-                        src: ['sounds/ufo.mp3']
-                    });
-                    break;
-                case 120:
-                    var sound = new Howl({
-                        src: ['sounds/veil.mp3']
-                    });
-                    break;
-                case 121:
-                    var sound = new Howl({
-                        src: ['sounds/wipe.mp3']
-                    });
-                    break;
-                case 122:
-                    var sound = new Howl({
-                        src: ['sounds/zig-zag.mp3']
-                    });
-                    break;
-                default: console.log("not defined");
-            }
-            sound.play();
+            //then play the sound
+            keyData[event.key].sound.play();
+            // and change body background color
+            $("body").css("background", createRndBgColor());
+            // and remove the hint
+            $("#hint").fadeOut(2000, function () {
+                this.remove()
+            });
         }
-    });
-
-
-
+    }
 }
